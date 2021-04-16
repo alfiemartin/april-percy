3af28db1683bcb2f2f90c5cb3b491d0bf6c7860e
alfiemartin <76059804+alfiemartin@users.noreply.github.com>
2021-04-16 15:39:06 +0100

    intro bare

--- components/IntroSection/IntroSection.js
+++ components/IntroSection/IntroSection.js
@@ -2,7 +2,17 @@ import React from "react";
 import styles from "./IntroSection.module.scss";
 
 const IntroSection = () => {
-  return <div className={styles.INTRO}></div>;
+  return (
+    <div className={styles.INTRO}>
+      <div className={styles.introGrid}>
+        <div className={styles.special}>Special Section</div>
+        <div className={styles.about}>About Me</div>
+        <div className={styles.projects}>Projects</div>
+        <div className={styles.text}>Hi, Welcome to my site!</div>
+        <div className={styles.contact}>Contact</div>
+      </div>
+    </div>
+  );
 };
 
 export default IntroSection;
--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -6,11 +6,11 @@ const NavBar = () => {
     <div className={styles.NAVBAR}>
       <div className={styles.leftSide}>
         <h1>A M</h1>
-        <h1>Section Title</h1>
+        <h1>Home</h1>
       </div>
       <div className={styles.rightSide}>
-        <h1>Socials</h1>
-        <h1>Email</h1>
+        {/* <h1>Socials</h1> */}
+        <h1>alfie.martin@hotmail.co.uk</h1>
       </div>
     </div>
   );
