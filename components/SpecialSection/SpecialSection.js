24825659d6b00a7fe6d8c9b157a6af5c5d69556b
alfiemartin <76059804+alfiemartin@users.noreply.github.com>
2021-04-18 19:58:46 +0100

    designing special section animations

--- components/SpecialSection/SpecialSection.js
+++ components/SpecialSection/SpecialSection.js
@@ -1,57 +1,81 @@
 import React, { useEffect, useRef } from "react";
-import gsap from "gsap";
+import gsap, { Power3 } from "gsap";
 import styles from "./SpecialSection.module.scss";
-
-const alfieMartinAnim = (alfieRef, martinRef, contRef) => {
-  const alfieAnim = gsap.timeline({
-    paused: true,
-    onComplete: () => alfieAnim.reverse(),
-  });
-
-  alfieAnim.to([alfieRef.current, martinRef.current], {
-    visibility: "visible",
-    duration: 0,
-    delay: 0,
-  });
-
-  alfieAnim.from(alfieRef.current, {
-    duration: 2,
-    x: contRef.current.clientWidth,
-    ease: "Power3.easeOut",
-    delay: 1,
-  });
-
-  alfieAnim.from(
-    martinRef.current,
-    {
-      duration: 2,
-      x: -contRef.current.clientWidth,
-      ease: "Power3.easeOut",
-    },
-    "<"
-  );
-
-  alfieAnim.to(
-    [alfieRef.current, martinRef.current],
-    {
-      textShadow: "3px 1px 17px rgba(17, 24, 39,.62)",
-      duration: 2.0,
-      ease: "Power3.easeInOut",
-    },
-    "<0.5"
-  );
-
-  return alfieAnim;
-};
 
 const SpecialSection = () => {
   let alfieRef = useRef();
   let martinRef = useRef();
   let contRef = useRef();
+  let webRef = useRef();
+  let devRef = useRef();
+  let elecRef = useRef();
+  let engRef = useRef();
 
   useEffect(() => {
-    const alfieMartinTimeline = alfieMartinAnim(alfieRef, martinRef, contRef);
-    alfieMartinTimeline.play();
+    const alfieAnim = gsap.timeline({
+      paused: true,
+      onComplete: () => alfieAnim.reverse(),
+      onReverseComplete: () => engineerAnim.play(),
+    });
+
+    const engineerAnim = gsap.timeline({
+      paused: true,
+      onComplete: () => engineerAnim.reverse(),
+      onReverseComplete: () => alfieAnim.play(),
+    });
+
+    alfieAnim.to([alfieRef.current, martinRef.current], {
+      visibility: "visible",
+      duration: 0,
+      delay: 0,
+    });
+    alfieAnim.from(alfieRef.current, {
+      duration: 2,
+      x: contRef.current.clientWidth,
+      ease: Power3.easeOut,
+      delay: 1,
+    });
+    alfieAnim.from(
+      martinRef.current,
+      {
+        duration: 2,
+        x: -contRef.current.clientWidth,
+        ease: Power3.easeOut,
+      },
+      "<"
+    );
+    alfieAnim.to(
+      [alfieRef.current, martinRef.current],
+      {
+        textShadow: "3px 1px 17px rgba(17, 24, 39,.62)",
+        duration: 2.0,
+        ease: Power3.easeInOut,
+      },
+      "<0.5"
+    );
+
+    engineerAnim.to(
+      [webRef.current, devRef.current, elecRef.current, engRef.current],
+      {
+        visibility: "visible",
+        duration: 0,
+        delay: 0,
+      }
+    );
+    engineerAnim.from(
+      [webRef.current, devRef.current, elecRef.current, engRef.current],
+      {
+        opacity: 0,
+        duration: 1,
+        ease: Power3.easeInOut,
+        stagger: {
+          amount: 1,
+          from: "random",
+        },
+      }
+    );
+
+    alfieAnim.play();
   }, []);
 
   return (
@@ -61,8 +85,14 @@ const SpecialSection = () => {
         <h1 ref={martinRef}>Martin</h1>
       </div>
       <div className={`${styles.textCont} ${styles.engineerText}`}>
-        <h1>Web Developer</h1>
-        <h1>Electronic Engineer</h1>
+        <div>
+          <h1 ref={webRef}>Web</h1>
+          <h1 ref={devRef}>Developer</h1>
+        </div>
+        <div>
+          <h1 ref={elecRef}>Electronic</h1>
+          <h1 ref={engRef}>Engineer</h1>
+        </div>
       </div>
     </div>
   );
