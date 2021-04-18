5f98824e106fe2a0a169a3ff6fb234c3c5be0c92
alfiemartin <76059804+alfiemartin@users.noreply.github.com>
2021-04-18 11:09:57 +0100

    made an animation function for intro text

--- components/SpecialSection/SpecialSection.js
+++ components/SpecialSection/SpecialSection.js
@@ -2,48 +2,51 @@ import React, { useEffect, useRef } from "react";
 import gsap from "gsap";
 import styles from "./SpecialSection.module.scss";
 
+const alfieMartinAnim = (alfieRef, martinRef, contRef) => {
+  const alfieAnim = gsap.timeline({
+    paused: true,
+    onComplete: () => alfieAnim.reverse(),
+  });
+
+  alfieAnim.to([alfieRef.current, martinRef.current], {
+    visibility: "visible",
+    duration: 0,
+    delay: 0,
+  });
+
+  alfieAnim.from(alfieRef.current, {
+    duration: 2,
+    x: contRef.current.clientWidth,
+    ease: "Power3.easeOut",
+    delay: 1,
+  });
+
+  alfieAnim.from(
+    martinRef.current,
+    {
+      duration: 2,
+      x: -contRef.current.clientWidth,
+      ease: "Power3.easeOut",
+    },
+    "<"
+  );
+
+  alfieAnim.to([alfieRef.current, martinRef.current], {
+    color: "#831843",
+    duration: 0.6,
+    ease: "Power3.easeOut",
+  });
+
+  return alfieAnim;
+};
+
 const SpecialSection = () => {
   let alfieRef = useRef();
   let martinRef = useRef();
   let contRef = useRef();
-  let tempRef = useRef();
 
   useEffect(() => {
-    const alfieAnim = gsap.timeline({
-      paused: true,
-      onComplete: () => alfieAnim.reverse(),
-    });
-
-    alfieAnim.to([alfieRef.current, martinRef.current], {
-      visibility: "visible",
-      duration: 0,
-      delay: 0,
-    });
-
-    alfieAnim.from(alfieRef.current, {
-      duration: 2,
-      x: contRef.current.clientWidth,
-      ease: "Power3.easeOut",
-      delay: 1,
-    });
-
-    alfieAnim.from(
-      martinRef.current,
-      {
-        duration: 2,
-        x: -contRef.current.clientWidth,
-        ease: "Power3.easeOut",
-      },
-      "<"
-    );
-
-    alfieAnim.to([alfieRef.current, martinRef.current], {
-      color: "#831843",
-      duration: 0.6,
-      ease: "Power3.easeOut",
-    });
-
-    alfieAnim.play();
+    alfieMartinAnim(alfieRef, martinRef, contRef).play();
   }, []);
 
   return (
