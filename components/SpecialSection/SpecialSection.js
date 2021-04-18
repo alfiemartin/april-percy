eb2f7a7a0348ce73435798ec2ead771658fb5f05
alfiemartin <76059804+alfiemartin@users.noreply.github.com>
2021-04-18 11:03:01 +0100

    pre trying to clean timeline

--- components/SpecialSection/SpecialSection.js
+++ components/SpecialSection/SpecialSection.js
@@ -1,10 +1,57 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
 import styles from "./SpecialSection.module.scss";
 
 const SpecialSection = () => {
+  let alfieRef = useRef();
+  let martinRef = useRef();
+  let contRef = useRef();
+  let tempRef = useRef();
+
+  useEffect(() => {
+    const alfieAnim = gsap.timeline({
+      paused: true,
+      onComplete: () => alfieAnim.reverse(),
+    });
+
+    alfieAnim.to([alfieRef.current, martinRef.current], {
+      visibility: "visible",
+      duration: 0,
+      delay: 0,
+    });
+
+    alfieAnim.from(alfieRef.current, {
+      duration: 2,
+      x: contRef.current.clientWidth,
+      ease: "Power3.easeOut",
+      delay: 1,
+    });
+
+    alfieAnim.from(
+      martinRef.current,
+      {
+        duration: 2,
+        x: -contRef.current.clientWidth,
+        ease: "Power3.easeOut",
+      },
+      "<"
+    );
+
+    alfieAnim.to([alfieRef.current, martinRef.current], {
+      color: "#831843",
+      duration: 0.6,
+      ease: "Power3.easeOut",
+    });
+
+    alfieAnim.play();
+  }, []);
+
   return (
-    <div className={styles.SPECIAL}>
-      <h1>Special Section</h1>
+    <div ref={contRef} className={styles.SPECIAL}>
+      <div className={styles.alfieTextCont}>
+        <h1 ref={alfieRef}>Alfie</h1>
+        <h1 ref={martinRef}>Martin</h1>
+      </div>
     </div>
   );
 };
