fbd41098ae9e5749d0d70336d836b31a8a0c9585
alfiemartin <76059804+alfiemartin@users.noreply.github.com>
2021-04-19 17:22:12 +0100

    projects links added cards and scrolling

--- components/Projects/Projects.js
+++ components/Projects/Projects.js
@@ -5,11 +5,28 @@ const NeurifyCard = () => {
   return (
     <div className={`${styles.projectCard} ${styles.neurify}`}>
       <h1>neurify</h1>
-      <p>React, CSS....</p>
-      {/* background image */}
+      <p>
+        neurify combines expert social media knowledge with technology to
+        deliver high quality campaigns.
+        <br />
+        <br />
+        Wordpress <br />
+        Elementor <br />
+        PHP <br />
+        JavaScript <br />
+        CSS
+      </p>
       <div className={styles.buttons}>
-        <button>Code</button>
-        <button>Live</button>
+        <button>
+          <a href="/" target="_blank">
+            Code Unavailable
+          </a>
+        </button>
+        <button>
+          <a href="https://www.neurify.co.uk/" target="_blank">
+            Live
+          </a>
+        </button>
       </div>
       <div className={styles.imageCont}>
         {/* <img src="dwyl.jpg" alt="" /> */}
@@ -22,11 +39,32 @@ const AlbaCard = () => {
   return (
     <div className={`${styles.projectCard} ${styles.alba}`}>
       <h1>Alba Mind</h1>
-      <p>React, CSS....</p>
-      {/* background image */}
+      <p>
+        The socially distant ad campaign for Alba Mind.
+        <br />
+        <br />
+        <br />
+        <br />
+        NextJS <br />
+        SCSS <br />
+        TypeScript <br />
+        MaterialUI <br />
+        Vercel
+      </p>
       <div className={styles.buttons}>
-        <button>Code</button>
-        <button>Live</button>
+        <button>
+          <a
+            href="https://github.com/alfiemartin/antisocially-distant"
+            target="_blank"
+          >
+            Code
+          </a>
+        </button>
+        <button>
+          <a href="https://socially-distant.me//" target="_blank">
+            Live
+          </a>
+        </button>
       </div>
       <div className={styles.imageCont}>
         {/* <img src="dwyl.jpg" alt="" /> */}
@@ -39,11 +77,29 @@ const RDMCard = () => {
   return (
     <div className={`${styles.projectCard} ${styles.rdm}`}>
       <h1>RDM Technology</h1>
-      <p>React, CSS....</p>
-      {/* background image */}
+      <p>
+        Redesign of the old website with a modern design and scroll animations.
+        <br />
+        <br />
+        <br />
+        NextJS <br />
+        TailwindCSS <br />
+        CSS
+      </p>
       <div className={styles.buttons}>
-        <button>Code</button>
-        <button>Live</button>
+        <button>
+          <a
+            href="https://github.com/alfiemartin/RDM-Technology"
+            target="_blank"
+          >
+            Code
+          </a>
+        </button>
+        <button>
+          <a href="http://www.rdmtechnology.com/" target="_blank">
+            Live
+          </a>
+        </button>
       </div>
       <div className={styles.imageCont}>
         {/* <img src="dwyl.jpg" alt="" /> */}
@@ -56,11 +112,29 @@ const TapFlashCard = () => {
   return (
     <div className={`${styles.projectCard} ${styles.tapflash}`}>
       <h1>Tap Flash</h1>
-      <p>React, CSS....</p>
+      <p>
+        A simple game I used to play as a kid, recreated using React.
+        <br />
+        <br />
+        <br />
+        <br />
+        ReactJS <br />
+        SCSS <br />
+        Firebase Hosting <br />
+        Firebase Firestore
+      </p>
       {/* background image */}
       <div className={styles.buttons}>
-        <button>Code</button>
-        <button>Live</button>
+        <button>
+          <a href="https://github.com/alfiemartin/TapFlash" target="_blank">
+            Code
+          </a>
+        </button>
+        <button>
+          <a href="https://tapflash-77edb.web.app/" target="_blank">
+            Live
+          </a>
+        </button>
       </div>
       <div className={styles.imageCont}>
         {/* <img src="dwyl.jpg" alt="" /> */}
@@ -73,11 +147,29 @@ const ChatroomCard = () => {
   return (
     <div className={`${styles.projectCard} ${styles.chatroom}`}>
       <h1>Chatroom</h1>
-      <p>React, CSS....</p>
+      <p>
+        A basic chatroom based off Firebase databases and also uses
+        authetication.
+        <br /> <br /> <br /> ReactJS <br />
+        SCSS <br />
+        Firebase Hosting <br />
+        Firebase Firestore
+      </p>
       {/* background image */}
       <div className={styles.buttons}>
-        <button>Code</button>
-        <button>Live</button>
+        <button>
+          <a
+            href="https://github.com/alfiemartin/Basic-Messaging-App"
+            target="_blank"
+          >
+            Code
+          </a>
+        </button>
+        <button>
+          <a href="https://chatroom-c36ed.web.app/" target="_blank">
+            Live
+          </a>
+        </button>
       </div>
       <div className={styles.imageCont}>
         {/* <img src="dwyl.jpg" alt="" /> */}
@@ -95,6 +187,12 @@ const Projects = () => {
         <RDMCard />
         <TapFlashCard />
         <ChatroomCard />
+      </div>
+      <div className={styles.messageSection}>
+        <h1>
+          Ongoing projects will be added to this list soon upon completion.{" "}
+        </h1>
+        <h2>Coming soon: Homepager (Homepage maker)</h2>
       </div>
     </div>
   );
