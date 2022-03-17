7fec1767f571c7cf3e6b7a9c24b9d17aef063d57
Alfie Martin <alfie.martin@remarkable.net>
2022-03-17 16:51:34 +0000

    tell users that a new site is coming

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-
 import styles from "../styles/Home.module.scss";
 import NavBar from "../components/NavBar/NavBar";
 import IntroSection from "../components/IntroSection/IntroSection";
@@ -7,6 +6,8 @@ import AboutMe from "../components/AboutMe/AboutMe";
 import Projects from "../components/Projects/Projects";
 import Contact from "../components/Contact/Contact";
 import { useRef, useState, useEffect } from "react";
+import Modal from "react-modal";
+import Cookies from "js-cookie";
 
 export default function Home() {
   const [sectionState, setSectionState] = useState("Home");
@@ -26,6 +27,15 @@ export default function Home() {
     });
   });
 
+  const alreadySeenModal = Cookies.get('modal-seen') === 'true';
+
+  const [showModal, setShowModal] = useState(!alreadySeenModal);
+  const handleModal = () => {
+    setShowModal(prev => !prev);
+
+    Cookies.set('modal-seen', 'true');
+  }
+
   return (
     <>
       <Head>
@@ -33,6 +43,13 @@ export default function Home() {
       </Head>
       <div className={styles.HOME}>
         <NavBar sectionState={sectionState} />
+        <Modal isOpen={showModal} style={{overlay: {width: "100vw", height: "100vh", display: 'grid', placeItems: 'center'}}} contentElement={() => (
+          <div style={{display: 'grid', placeItems: 'center'}}>
+            <h1>New site coming soon 😩😩</h1>
+            <h2 style={{textDecoration: 'underline', marginBottom: '60px'}}><a href="https://portfolio-2022-zeta.vercel.app/" target="_blank">Click here to see a preview</a></h2>
+            <button onClick={handleModal} style={{border: 'none', background: 'none'}}><h2 style={{textDecoration: 'underline'}}>Close this message</h2></button>
+          </div>
+        )} />
         <IntroSection
           aboutRef={aboutRef}
           projectsRef={projectsRef}
